c41603eeec51d538966a8d8fa1e9e3b8d4dbc184
Christoffel Kweknoto <emperorchrisz@gmail.com>
2021-11-30 16:36:02 +0700

    Mengerjakan Layout pada halaman favorite dan artikel Membuat articlelist untuk recyclerview pada artikel mengerjakan design UI dengan xml pada halaman favorite dan juga halaman article menghapus beberapa kode yang tidak diperlukan menambah beberapa design untuk itemlist

--- app/src/main/java/com/capstone/sifood/ui/article/ArticleFragment.kt
+++ app/src/main/java/com/capstone/sifood/ui/article/ArticleFragment.kt
@@ -29,10 +29,6 @@ class ArticleFragment : Fragment() {
         _binding = FragmentArticleBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner, {
-            textView.text = it
-        })
         return root
     }
 
--- app/src/main/java/com/capstone/sifood/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/capstone/sifood/ui/favorite/FavoriteFragment.kt
@@ -30,10 +30,7 @@ class FavoriteFragment : Fragment() {
         _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
+
         return root
     }
 
